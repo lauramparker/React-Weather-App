8ad8f149d7d5b444dfa8c64e3541b44a55646a57
Laura Parker <lauramparker78@gmail.com>
2021-04-13 09:00:03 -0400

    Adding / editing API utils with url queries

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,32 +1,26 @@
 import React, { useContext } from 'react';
+import { WeatherContext } from '../providers/WeatherProvider';
 import axios from 'axios';
 
+const { searchCity } = useContext(WeatherContext);
 
-
-//Open Weather Map API keys
-//Current forecast APIKEY
-var APIKey = "1b758f2281f1833aa291dff536f4b566"; 
-
-//5 day forecast API Key
-var fivedayAPIKey = "65af81772398c8021de436a5afa38da3"; 
-
-//Current UV Index API Key
-var uvIndexAPIKey = "1f3df3f7624cfc5a7a0d585bce621279"
-      
-var currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=1b758f2281f1833aa291dff536f4b566";
-
-var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + searchCity + "&cnt=5&appid=65af81772398c8021de436a5afa38da3";
-
-export default {
-
+const API = {
     
+    //Open Weather Map API keys
+    // var APIKey = "1b758f2281f1833aa291dff536f4b566";  //Current forecast APIKEY
+    // var fivedayAPIKey = "65af81772398c8021de436a5afa38da3";  //5 day forecast API Key
+    // var uvIndexAPIKey = "1f3df3f7624cfc5a7a0d585bce621279" //Current UV Index API Key
+    // var currentWeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=1b758f2281f1833aa291dff536f4b566";
+    // var fiveDayURL = "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + searchCity + "&cnt=5&appid=65af81772398c8021de436a5afa38da3";
+
     getCurrentWeather: function() {
-        return
-            axios.get(currentWeatherURL)
+        return axios.get("https://api.openweathermap.org/data/2.5/weather?q=" + searchCity + "&appid=1b758f2281f1833aa291dff536f4b566")
+        
     },
 
     getFiveDay: function() {
-        return
-            axios.get(fiveDayURL)
+        return axios.get("https://api.openweathermap.org/data/2.5/forecast/daily?q=" + searchCity + "&cnt=5&appid=65af81772398c8021de436a5afa38da3")
     }
 }
+
+export default API;
