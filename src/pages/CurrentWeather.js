fc7c7b8a7b9e82a24623141fb38a79e8186fb22f
Laura Parker <lauramparker78@gmail.com>
2021-04-14 12:47:27 -0400

    Editing useEffect to address unmounted component error

--- src/pages/CurrentWeather.js
+++ src/pages/CurrentWeather.js
@@ -2,36 +2,43 @@ import React, { useEffect, useContext } from 'react';
 import { WeatherContext } from '../providers/WeatherProvider';
 import {Row, Col, Container} from 'react-bootstrap';
 import API from '../utils/API';
+import Search from '../components/Search';
 
 const CurrentWeather = () => {
 
-    const { currentWeather, setCurrentWeather } = useContext(WeatherContext);
+    const { currentWeather, setCurrentWeather, searchCity } = useContext(WeatherContext);
 
-    // const [newWeather, setNewWeather] = useState({});
 
     useEffect(() => {
-        API.getCurrentWeather()   //params (searchCity)?
-        .then(res => setCurrentWeather({
-            name: res.name,
-            // date: moment.unix((res.dt).format("MM/DD/YYYY")),
-            temp: res.main.temp,
-            humidity: res.main.humidity,
-            wind: res.wind.speed,
-            icon: res.weather[0].icon,
-            lat: res.coord.lat,
-            long: res.coord.lon
-        }))
-        .catch(err => console.error(err));
-      }, [setCurrentWeather]);
-
+            let isMounted = true; // tip from Stack Overflow: 53949393/cant-perform-a-react-state-update-on-an-unmounted-component
+            API.getCurrentWeather(searchCity).then(res => {
+              if (isMounted) setCurrentWeather({
+                name: res.data.name,
+                // date: moment.unix((res.dt).format("MM/DD/YYYY")),
+                temp: res.data.main.temp,
+                humidity: res.data.main.humidity,
+                wind: res.data.wind.speed,
+                icon: res.data.weather[0].icon,
+                lat: res.data.coord.lat,
+                long: res.data.coord.lon
+              });
+            }).catch(err => console.error(err));
+            return () => { isMounted = false }; // use effect cleanup to set flag false, if unmounted
+          });
 
     return(
         <Container>
+               <Row>
+                <Col>
+                    <Search />
+                </Col>
+            </Row>
             <Row>
                 <Col>
-                 <h4>name: {currentWeather.name} {currentWeather.icon}</h4>
+                <p>NAME of CITY</p>
+                 {/* <h4>name: {currentWeather.name} {currentWeather.icon}</h4>
                  <p>temperature: {currentWeather.temp}</p>
-                 <p>humidity: {currentWeather.humidity}</p>
+                 <p>humidity: {currentWeather.humidity}</p> */}
                 </Col>
             </Row>
         </Container>
