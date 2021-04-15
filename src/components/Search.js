8bfa08797d91efa6f85e65693c120f7491c53fce
Laura Parker <lauramparker78@gmail.com>
2021-04-14 23:28:53 -0400

    Editing Search to work properly & update state in provider

--- src/components/Search.js
+++ src/components/Search.js
@@ -2,34 +2,28 @@ import React, { useState, useContext, useEffect } from 'react';
 import { Row, Col, Container } from 'react-bootstrap';
 import { WeatherContext } from '../providers/WeatherProvider';
 
-const Search = () => {  //{children}
+const Search = ({children}) => { 
     //from WeatherContext
-    const { setSearchCity, setSearchList } = useContext(WeatherContext);
+    const { searchCity, setSearchCity, setSearchList, searchList } = useContext(WeatherContext);
 
-    //assignment to read value of current input city name
-    const [name, setName] = useState('');
-
-    //could also do this in input line as    onChange={(event) => setName(event.target.value)}
-    const handleChange = (event) => {
-        setName({value: event.target.value});
-      };
+    //use component state inherent to search form
+    const [name, setName] = useState(null);
 
     const handleSubmit = (event) => {
-        console.log('Searching for this city: ' + name);
         event.preventDefault();
+        setSearchCity(name);
+        setSearchList(prevSearchList => ([...prevSearchList, name]));
         if (!name) {
             console.log('oops! City Name not entered')
         }
     }; //end handleSubmit
 
-    //then useEffect (anonymous function) with [dependency] required as callback to update 
-    //update both searchCity and searchList
+    //then useEffect (anonymous function) with [dependencies] required as callback to update 
     useEffect(() => {
         if (name) {
-            setSearchCity({ cityName: name })
-            setSearchList({ ...setSearchList.push(name)}) //also maybe?  setSearchList({searchList: [setSearchList, name]})
-        }
-    }, [name]);
+            console.log('Searching for this city: ' + searchCity + searchList)
+        } else {console.log('Nooooope!')}
+    }, [name, setSearchCity, searchList]);
 
 
     return(
@@ -38,8 +32,14 @@ const Search = () => {  //{children}
                 <Col>
                 <form onSubmit={handleSubmit}>
                 <label>
-                City:
-                <input type="text" value={name} placeholder="search for a city..." onChange={handleChange} />
+                City:{searchCity}
+                <input 
+                  type="text" 
+                  name="cityName"
+                  value={name} 
+                  placeholder="search for a city..." 
+                  onChange={(event) => setName(event.target.value)}
+                  />{children}
                 </label>
                 <input type="submit" value="Submit" />
                 </form>
