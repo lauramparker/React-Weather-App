e25ec9bc362465baf0032c5f52a4b71a27a9e6d3
Laura Parker <lauramparker78@gmail.com>
2021-04-26 08:10:40 -0400

    Adding Table component and hourly forecast

--- src/components/Table.js
+++ src/components/Table.js
@@ -4,21 +4,21 @@ import { WeatherContext } from '../providers/WeatherProvider';
 import Moment from 'react-moment';
 
 
-const CardContainer = ({children}) => {
+const Table = ({children}) => {
 
     const { sevenDayWeather } = useContext(WeatherContext);
 
     return(
-        <Container id="sevenDayBox"> {children}
-            <div className="wrapper-sevenDay">
-                {sevenDayWeather.data && sevenDayWeather.data.slice(1).map(daily => { //slice(1) starts the map from the second array element
+        <Container id="hourlyBox"> {children}
+            <div className="table-hourly">
+                {sevenDayWeather.hourly && sevenDayWeather.hourly.slice(1).map(hourly => { //slice(1) starts the map from the second array element
                     return(
-                        <div className="daily" key={sevenDayWeather.data.dt}>
-                            <Moment unix format="dddd, MMMM Do">{((daily.dt))}</Moment>
+                        <div className="daily" key={sevenDayWeather.hourly.dt}>
+                            <Moment unix format="hh:mm">{((hourly.dt))}</Moment>
                             <Row>
                                 <Col>
                                 <img
-                                src={'https://openweathermap.org/img/wn/'+daily.weather[0].icon+'@2x.png'}
+                                src={'https://openweathermap.org/img/wn/'+hourly.weather[0].icon+'@2x.png'}
                                 alt="weather icon"
                                 />
                                 </Col>
@@ -38,4 +38,4 @@ const CardContainer = ({children}) => {
     )
 }
 
-export default CardContainer;
+export default Table;
